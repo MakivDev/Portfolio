26b37ef7da69c50d5aed127e6fc576a6b1a1d861
MakivDev <8064637@stud.nau.edu.ua>
2024-09-08 02:11:50 +0300

    AboutPage1

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Footer from './components/footer/Footer';
 
 import Home from './pages/Home';
 import Projects from './pages/Projects';
+import About from './pages/About';
 
 
 function App() {
@@ -14,7 +15,9 @@ function App() {
     <div className="App">
       <Navbar />
       {/* <Home /> */}
-      <Projects/>
+      {/* <Projects/> */}
+      <About/>
+
 
       <Footer />
 
